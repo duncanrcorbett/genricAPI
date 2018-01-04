38795b6a9036d4ce908a532e2e08fe52063015a4
Duncan Corbett <duncanrcorbett@gmail.com>
2018-01-04 15:32:44 +0200

    added PUT request

--- server.js
+++ server.js
@@ -61,6 +61,34 @@ app.delete('/allData/:id', function (req,res){
     }
 });
 
+// PUT /allData/:id
+app.put('/allData/:id', function (req, res){
+    var body = _.pick(req.body,'description','completed' );
+    var validAttributes = {};
+    var itemID = parseInt(req.params.id, 10);
+    var matchedItem = _.findWhere(dataItems, {id: itemID});
+
+    if (!matchedItem){
+        return res.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedItem, validAttributes);
+    res.json(matchedItem);
+
+});
+
 app.listen(PORT, function() {
     console.log('Express listening on port '+ PORT );
 });
